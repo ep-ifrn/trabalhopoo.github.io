e6017773419f29551389a5fcd708f82ee5b4729e
Enzo G <enzo.pontes321@gmail.com>
2026-08-19 17:07:03 -0300

    feat(exercicios): adiciona solucao dos operadores matematicos e de comparacao

---
+++ atividade01_19082026/index.py
@@ -0,0 +1,19 @@
+#Solicita os dois números inteiros
+A = int(input("Digite o valor de A:"))
+B = int(input("Digite o valor de B: "))
+
+#Exibe o resultado das contas
+print(f"A + B = {A + B}")
+print(f"A * B = {A * B}")
+print(f"A - B = {A - B}")
+print(f"A / B = {A / B}")
+
+# Solicita os dois números inteiros
+A = int(input("Digite o valor de A: "))
+B = int(input("Digite o valor de B: "))
+
+# Exibe o resultado das comparações (retorna True ou False)
+print(f"A > B = {A > B}")
+print(f"A <= B = {A <= B}")
+print(f"A != B = {A != B}")
+print(f"A == B = {A == B}")
